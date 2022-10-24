ea56f56b05ff1ab826ed8909aac462536c80149a
yzyzzg <1307330146@qq.com>
2022-10-25 00:26:41 +0800

    Add eulur

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<math.h>
+#define f(y,x) (-y)
+int main()
+{
+	int a,i;
+	double y1,h,x,t;
+	printf("初值a：");scanf("%d", &a); 
 
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+	
+	y1=a;
+	h=0.001;
+	
+	printf("x\t\ty(欧拉)\n");
+	printf("%lf\t%lf\n",x,y1);
+	//欧拉法计算 
+	do{
+		t=y1;
+		y1=y1+h*f(y1,x);
+		x=x+h;
+		printf("%lf\t%lf\n",x,y1);
+		}while(fabs(t-y1)>= 0.001*h);
+	
+	printf("收敛值为%lf",y1);
+	
 	return 0;
-}
-
+ } 
